cfb07bc26f0d0240383f5ff2810b907cfbcca184
amerdahabi <aamerdahabi@gmail.com>
2020-08-19 16:10:34 +0200

    add public folder

--- src/user.context.js
+++ src/user.context.js
@@ -1,27 +1,26 @@
-import React from "react";	
+import React from "react";
 
-export const UserContext = React.createContext();	
+export const UserContext = React.createContext();
 
-class UserProvider extends React.Component {	
-  state = {	
-    users: [],	
-    loading: false,	
-    error: null, 	
-  };	
+class UserProvider extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+    error: null,
+  };
 
-  updateUserContext = (value) => {	
-    this.setState(value)	
-  }  	
+  updateUserContext = (value) => {
+    this.setState(value);
+  };
 
-  render() {	
-    return (	
-      <UserContext.Provider	
-        value={{ ...this.state, updateUserContext: this.updateUserContext }}	
-      >	
-        {this.props.children}	
-      </UserContext.Provider>	
-    );	
-  }	
-}	
- export default UserProvider	
-
+  render() {
+    return (
+      <UserContext.Provider
+        value={{ ...this.state, updateUserContext: this.updateUserContext }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
+export default UserProvider;
--- src/users.js
+++ src/users.js
@@ -20,11 +20,8 @@ const Users = () => {
   return (
     <ul className="users-list">
       {users.map((user) => (
-        // <li key={user.id} className="user-item">
-        //   <div>User name: {user.login}</div>
-        // </li>
         <li key={user.id} className="user-item">
-          <Card style={{ width: "18rem" }}>
+          <Card style={{ width: "40rem" }}>
             <Card.Img variant="top" src={user.avatar_url} />
             <Card.Body>
               <Card.Title>User name: {user.login}</Card.Title>
@@ -40,23 +37,3 @@ const Users = () => {
 };
 
 export default Users;
-
-// avatar_url: "https://avatars0.githubusercontent.com/u/9213230?v=4"
-// events_url: "https://api.github.com/users/cagataycali/events{/privacy}"
-// followers_url: "https://api.github.com/users/cagataycali/followers"
-// following_url: "https://api.github.com/users/cagataycali/following{/other_user}"
-// gists_url: "https://api.github.com/users/cagataycali/gists{/gist_id}"
-// gravatar_id: ""
-// html_url: "https://github.com/cagataycali"
-// id: 9213230
-// login: "cagataycali"
-// node_id: "MDQ6VXNlcjkyMTMyMzA="
-// organizations_url: "https://api.github.com/users/cagataycali/orgs"
-// received_events_url: "https://api.github.com/users/cagataycali/received_events"
-// repos_url: "https://api.github.com/users/cagataycali/repos"
-// score: 1
-// site_admin: false
-// starred_url: "https://api.github.com/users/cagataycali/starred{/owner}{/repo}"
-// subscriptions_url: "https://api.github.com/users/cagataycali/subscriptions"
-// type: "User"
-// url: "https://api.github.com/users/cagataycali"
